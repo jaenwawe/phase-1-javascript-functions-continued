3da1b499b48fca34402e651abe8d80ae9b75709f
jaenwawe <nwawe.jae@gmail.com>
2021-08-16 13:15:58 -0500

    commit tree a5e82e2726d88e6a706100222b839debacb63eb2

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Your code here
+function razzle(lawyer="Billy", target="'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}!`);
+  }
+
+  const satFun = "run";
+  razzle();
+  saturdayFun (satFun);
+
+  function saturdayFun (word = "roller-skate"){
+  
+    return `This Saturday, I want to ${word}!`;
+  }
+
+
+  const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+  }
+
+  // function(baseNumber){ return baseNumber + 3 })(2);
+
+
+  const array = (
+    function(thingToAdd) {
+      const base = 3;
+      return [
+        function() { return base + thingToAdd; },
+        function() { return base; }
+      ];
+    }
+  )(2)
+
+  console.log(array[0])
+  console.log(array[1])
+
+  console.log(array[0]())
+  console.log(array[1]())
+  
+  
+  
+  function wrapAdjective (flair) {
+
+  if(flair == null)
+  return (single = "special", flair = 
+      "*") =>  { return `You are ${flair}${single}${flair}!`}
+
+  if (flair == "||")
+  return (single = "a dedicated programmer", flair = "||") =>  { return `You are ${flair}${single}${flair}!`}
+  }
+
